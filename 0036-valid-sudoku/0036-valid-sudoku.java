1117ec59e1ca030e61d76e75fa0ccb5f41567b24
Samarth Shendre <156545091+i-m-samarth-cs@users.noreply.github.com>
2025-08-30 12:01:21 +0530

    Time: 1 ms (99.87%), Space: 44.3 MB (78.74%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,28 +1,23 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        boolean[] rows[] = new boolean[9][9];
-        boolean[] columns[] = new boolean[9][9];
-        boolean[] boxes[] = new boolean[9][9];
+        boolean[][] rows = new boolean[9][9];
+        boolean[][] cols = new boolean[9][9];
+        boolean[][] boxes = new boolean[9][9];
 
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
-                char num = board[i][j];
-                if (num != '.') {
-                    int index = num - '1'; // Convert char to index
-                    int boxIndex = (i / 3) * 3 + (j / 3); // Determine the box index
+                if (board[i][j] != '.') {
+                    int num = board[i][j] - '1';
+                    int boxIndex = (i / 3) * 3 + (j / 3);
 
-                    // Check if the number has already been seen in the current row, column, or box
-                    if (rows[i][index] || columns[j][index] || boxes[boxIndex][index]) {
+                    if (rows[i][num] || cols[j][num] || boxes[boxIndex][num]) {
                         return false;
                     }
 
-                    // Mark the number as seen
-                    rows[i][index] = true;
-                    columns[j][index] = true;
-                    boxes[boxIndex][index] = true;
+                    rows[i][num] = cols[j][num] = boxes[boxIndex][num] = true;
                 }
             }
         }
-        return true; // If all checks passed, the Sudoku is valid
+        return true;
     }
-} 
+}
